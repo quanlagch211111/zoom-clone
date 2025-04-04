41c1f9225311539708182d47a2ef08d0dcb591d4
Quan <quanlagch211111@fpt.edu.vn>
2025-04-04 22:49:53 +0700

    change port

--- server.js
+++ server.js
@@ -34,4 +34,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(process.env.PORT || 3030, "0.0.0.0", () => {
+  console.log(`Server is running on port ${process.env.PORT || 3030}`);
+});
